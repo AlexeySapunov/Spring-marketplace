5c355b8caefc00b139c8d466e97885af6582322c
Alexey <blacka8607@rambler.ru>
2022-03-03 16:14:54 +0300

    homework to lesson 3

---
+++ shop-frontend-app/src/app/page/product-gallery-page/product-gallery-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-gallery-page',
+  templateUrl: './product-gallery-page.component.html',
+  styleUrls: ['./product-gallery-page.component.less']
+})
+export class ProductGalleryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
